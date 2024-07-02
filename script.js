de285778cc58f337e1f9067e5cb8821acbac333e
Kalyani Bajirao <95849305+kalyanibajirao@users.noreply.github.com>
2024-07-02 22:28:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+document.getElementById('btn').addEventListener('click', async () => {
+  const text = document.getElementById('text').value;
+  const delay = parseInt(document.getElementById('delay').value);
+
+  if (!isNaN(delay)) {
+    await delayMessage(delay);
+    document.getElementById('output').textContent = text;
+  } else {
+    alert('Please enter a valid delay');
+  }
+});
+
+function delayMessage(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
